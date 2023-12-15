9e1c71a79ac0cb3a6faa76a8d546fd1fb2312856
J <101463395+Jani4942@users.noreply.github.com>
2023-12-16 00:10:54 +0800

    Add files via upload

---
+++ README.md
@@ -0,0 +1,14 @@
+# 一键申请Let's Encrypt域名证书
+
+## Linux Only | NGINX
+
+### 使用:
+
+申请证书前请提前解析域名到服务器
+
+```
+bash <(curl -s -L git.io/dmSSL)
+```
+
+按照提示输入域名即可
+证书文件保存在`/home/ssl/<domain>/`路径下
---
+++ domainSSL.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+read -p "请输入域名:" domain && cd /tmp
+if [ ! -f "lego_v3.8.0_freebsd_amd64.tar.gz" ]; then
+  wget https://cdn.jsdelivr.net/gh/moeik/domainSSL@master/lego/lego_v3.8.0_linux_amd64.tar.gz
+fi
+tar zxvf lego_v3.8.0_linux_amd64.tar.gz
+chmod 755 *
+service nginx stop
+./lego --email="admin@$domain" --domains="$domain" --http -a run
+service nginx start
+if ls ./.lego/certificates | grep "$domain"
+    then
+    mkdir -p /home/ssl/$domain
+    cp ./.lego/certificates/$domain.crt /etc/x-ui/server.crt
+    cp ./.lego/certificates/$domain.key /etc/x-ui/server.key
+    path="/home/ssl/$domain/"
+    echo '证书签发成功，证书文件保存在'$path'。'
+else
+    echo '证书签发失败，请检查80端口是否被占用，域名解析或者输入域名是否正确。'
+fi
